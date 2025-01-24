6f372807110347daffda2c7d13fbf34315d6e405
Roman Zabolotin <rzabolotin@gmail.com>
2025-01-25 00:35:37 +1300

    child page

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,10 +1,5 @@
 from fastapi import APIRouter
 
-# from app.api.v1.auth import router as auth_router
-# from app.api.v1.children import router as children_router
-# from app.api.v1.tasks import router as tasks_router
-# from app.api.v1.rewards import router as rewards_router
-
 from app.api.v1 import (
     auth,
     children,
